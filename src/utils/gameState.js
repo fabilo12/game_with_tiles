8b41609d36cebc9f17ad67a344d0a77d38854f02
Fabian Lorenz <lorenz@imbi.uni-freiburg.de>
2023-01-30 12:53:27 +0100

    refactor gameState as function that returns object

--- src/utils/gameState.js
+++ src/utils/gameState.js
@@ -10,65 +10,69 @@ const EMPTY_TILE_FIELD = -1;
 const NAME_LIST_DEFAULT = ['Player 1', 'Player 2', 'Player 3', 'Player 4'];
 const NUM_PLAYER_DEFAULT = 4;
 
-export default function gameState(
-	playerList
-) {
-	//game hyperparameters
-	this.nColor = N_COLORS;
-	this.tileColorNames = TILE_COLOR_NAMES;
-	this.tilesPerRepo = TILES_PER_REPO;
-	this.tilesPerColor = TILES_PER_COLOR;
-	this.emptyTileField = EMPTY_TILE_FIELD;
-	this.nameListDefault = [...NAME_LIST_DEFAULT];
-	this.numPlayerDefault = NUM_PLAYER_DEFAULT;
+export default function gameState(playerListArg) {
 
-	//state shared between players
-	this.nameListInput = (typeof(playerList) === 'undefined') ? 
-		this.nameListDefault.slice(0, this.numPlayerDefault) : 
-		[...playerList];
-	this.playerList = (typeof(playerList) === 'undefined') ? 
-		this.nameListDefault.slice(0, this.numPlayerDefault) : 
-		[...playerList];
-	this.numPlayerSelected = this.playerList.length;
-	this.nPlayer = this.playerList.length;
-	this.nRepos = 2 * this.nPlayer + 1;
-	this.isGameOver = false;
-	this.bag = Array.from(
-		Array(N_COLORS), (_, i) => Array(TILES_PER_COLOR).fill(i)
-	).flat();
-	this.usedTiles = [];
-	this.repos = Array(this.nRepos).fill().map(
-		_ => Array(TILES_PER_REPO).fill(EMPTY_TILE_FIELD)
-	);
-	this.repos[this.nRepos] = [];//last repo is 'the middle', initially empty;
-	this.startingTile = 'middle';
-	this.playerAtTurn = getRandomInt(0, this.nPlayer)[0];
-	this.playMode = "predefined";
-	this.wallPattern = Array.from(
-		Array(N_COLORS),
-		(_, j) => Array.from(
+	const playerListAux = (typeof (playerListArg) === 'undefined') ?
+		[...NAME_LIST_DEFAULT].slice(0, NUM_PLAYER_DEFAULT) :
+		[...playerListArg];
+	const nPlayerAux = playerListAux.length;
+	const nReposAux = 2 * nPlayerAux + 1;
+
+	const initState = {
+		//game hyperparameters
+		nColor: N_COLORS,
+		tileColorNames: TILE_COLOR_NAMES,
+		tilesPerRepo: TILES_PER_REPO,
+		tilesPerColor: TILES_PER_COLOR,
+		emptyTileField: EMPTY_TILE_FIELD,
+		nameListDefault: [...NAME_LIST_DEFAULT],
+		numPlayerDefault: NUM_PLAYER_DEFAULT,
+
+		//state shared between players
+		nameListInput: (typeof (playerListArg) === 'undefined') ?
+			[...NAME_LIST_DEFAULT].slice(0, NUM_PLAYER_DEFAULT) :
+			[...playerListArg],
+		playerList: playerListAux,
+		numPlayerSelected: nPlayerAux,
+		nPlayer: nPlayerAux,
+		nRepos: nReposAux,
+		isGameOver: false,
+		bag: Array.from(
+			Array(N_COLORS), (_, i) => Array(TILES_PER_COLOR).fill(i)
+		).flat(),
+		usedTiles: [],
+		repos: Array(nReposAux + 1).fill().map(
+			(_, i) => i < (nReposAux) ?
+				Array(TILES_PER_REPO).fill(EMPTY_TILE_FIELD) :
+				Array(0)
+		),//last repo is 'the middle', initially empty,
+		startingTile: 'middle',
+		playerAtTurn: getRandomInt(0, nPlayerAux)[0],
+		wallPattern: Array.from(
 			Array(N_COLORS),
-			(_, i) => (N_COLORS - i + j) % N_COLORS
-		)
-	);
+			(_, j) => Array.from(
+				Array(N_COLORS),
+				(_, i) => (N_COLORS - i + j) % N_COLORS
+			)
+		),
 
-	//player specific state
-	this.scores = Array(this.nPlayer).fill(0);
-	this.rows = Array(this.nPlayer).fill().map(
-		_ => Array.from(
-			Array(N_COLORS), (_, i) => Array(i + 1).fill(EMPTY_TILE_FIELD)
-		)
-	);
-	this.wall = Array(this.nPlayer).fill().map(
-		_ => Array.from(
-			Array(N_COLORS), _ => Array(N_COLORS).fill(EMPTY_TILE_FIELD)
-		)
-	);
-	this.penalties = Array(this.nPlayer).fill().map(_ => []);
+		//player specific state
+		scores: Array(nPlayerAux).fill(0),
+		rows: Array(nPlayerAux).fill().map(
+			_ => Array.from(
+				Array(N_COLORS), (_, i) => Array(i + 1).fill(EMPTY_TILE_FIELD)
+			)
+		),
+		wall: Array(nPlayerAux).fill().map(
+			_ => Array.from(
+				Array(N_COLORS), _ => Array(N_COLORS).fill(EMPTY_TILE_FIELD)
+			)
+		),
+		penalties: Array(nPlayerAux).fill().map(_ => [])
+	};
 
-	//fill repos separately to facilitate reuse of fillRepos
-	const filledRepoState = fillRepos(this);
-	this.repos = filledRepoState.repos;
-	this.bag = filledRepoState.bag;
-	this.usedTiles = filledRepoState.usedTiles;
+	console.log(initState.repos.concat([]))
+	const initStateFilledRepo = fillRepos(initState);
+
+	return initStateFilledRepo
 };
